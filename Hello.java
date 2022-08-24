baf4f08f09d90158ba006d33d21f612f22720736
Soma-Cloud <112000581+Soma-Cloud@users.noreply.github.com>
2022-08-24 11:38:44 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**********************
+*Hello
+***********************
+*Noah Walden
+*August 24th, 2022
+*CMSC255
+**********************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Noah Walden");
+		System.out.println("Hello");
+		System.out.println("Print student name and information designatd by the professor");
+		System.out.println("August 24th, 2022");
+		System.out.println("CMSC255");
+		System.out.println();
+	}
+}
